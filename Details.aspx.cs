32e948fc3b5d059b4df325f1065b1a70dbf61fe7
Xiang Xingzhou <906551562@qq.com>
2022-07-05 10:59:37 +0800

    detail to cart add and update

--- Details.aspx.cs
+++ Details.aspx.cs
@@ -11,13 +11,13 @@ namespace Group_Project
 {
     public partial class Details : System.Web.UI.Page
     {
+        String name;
         protected void Page_Load(object sender, EventArgs e)
         {
             SqlConnection con = new SqlConnection(@"Data Source=TS\SQLEXPRESS;Initial Catalog=shoestore;Integrated Security=True");
             con.Open();
             String initialization = "select * from product where PRODUCT_ID ='" + Session["ProductID"] + "'";
             SqlCommand cmd = new SqlCommand(initialization, con);
-            //DataSet ds = new DataSet();
             SqlDataReader dr = cmd.ExecuteReader();
             String image;
             decimal price;
@@ -44,23 +44,54 @@ namespace Group_Project
         {
             try
             {
+                name = "Bob";
                 SqlConnection con = new SqlConnection(@"Data Source=TS\SQLEXPRESS;Initial Catalog=shoestore;Integrated Security=True");
                 con.Open();
-                String insertOrder = "insert into carts (ORDER_USERNAME, ORDER_PRODUCT_NAME, ORDER_PRODUCT_PRICE, ORDER_PRODUCT_IMAGE, ORDER_NUMBER) " +
+                String selectOrder = "select * from carts where ORDER_USERNAME = '" + name + "'"
+                    + " and ORDER_PRODUCT_NAME = '" + Label1.Text + "'";
+                SqlCommand cmd1 = new SqlCommand(selectOrder, con);
+                SqlDataReader dr = cmd1.ExecuteReader();
+                bool insertNew = false;
+
+
+                if (dr.Read())
+                {
+                    insertNew = true;
+                    String updateOrder = "update carts set ORDER_NUMBER = " + DropDownList1.SelectedValue
+                        + " where ORDER_USERNAME = '" + name + "'"
+                        + " and ORDER_PRODUCT_NAME = '" + Label1.Text + "'";
+                    SqlCommand cmd2 = new SqlCommand(updateOrder, con);
+
+                    dr.Close();
+                    cmd2.ExecuteNonQuery();
+                }
+                else
+                {
+                    String insertOrder = "insert into carts (ORDER_USERNAME, ORDER_PRODUCT_NAME, ORDER_PRODUCT_PRICE, ORDER_PRODUCT_IMAGE, ORDER_NUMBER) " +
                     "values (@USERNAME, @PRODUCT_NAME, @PRODUCT_PRICE, @PRODUCT_IMAGE, @NUMBER)";
-                SqlCommand cmd = new SqlCommand(insertOrder, con);
+                    SqlCommand cmd2 = new SqlCommand(insertOrder, con);
+                    dr.Close();
 
-                cmd.Parameters.AddWithValue("@USERNAME", "Bob");
-                //cmd.Parameters.AddWithValue("@USERNAME", XXX.Text);
-                cmd.Parameters.AddWithValue("@PRODUCT_NAME", Label1.Text);
-                cmd.Parameters.AddWithValue("@PRODUCT_PRICE", Label3.Text.Substring(2));
-                cmd.Parameters.AddWithValue("@PRODUCT_IMAGE", ProductImage.ImageUrl);
-                cmd.Parameters.AddWithValue("@NUMBER", DropDownList1.SelectedValue);
-                cmd.ExecuteNonQuery();
+                    cmd2.Parameters.AddWithValue("@USERNAME", "Bob");
+                    //cmd.Parameters.AddWithValue("@USERNAME", XXX.Text);
+                    cmd2.Parameters.AddWithValue("@PRODUCT_NAME", Label1.Text);
+                    cmd2.Parameters.AddWithValue("@PRODUCT_PRICE", Label3.Text.Substring(2));
+                    cmd2.Parameters.AddWithValue("@PRODUCT_IMAGE", ProductImage.ImageUrl);
+                    cmd2.Parameters.AddWithValue("@NUMBER", DropDownList1.SelectedValue);
+                    cmd2.ExecuteNonQuery();
+                }
+
+                
                 con.Close();
+                if (insertNew)
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "alert", "<script>alert('Successfully add to cart!');</script>");
+                }
+                else
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "alert", "<script>alert('Successfully update the cart!');</script>");
+                }
 
-
-                Page.ClientScript.RegisterStartupScript(this.GetType(), "alert", "<script>alert('Successfully add to cart!');</script>");
                 if (int.Parse((String)Session["ProductID"]) <=4 || (8<=int.Parse((String)Session["ProductID"]) && int.Parse((String)Session["ProductID"]) <= 11))
                 {
                     Response.Redirect("Man.aspx");
