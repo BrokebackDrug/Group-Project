608ad40f2421ada69444188f767b3efff5224949
Xiang Xingzhou <906551562@qq.com>
2022-07-06 20:42:55 +0800

    final

--- Details.aspx.cs
+++ Details.aspx.cs
@@ -6,7 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data;
-using System.Windows;
+using System.Windows.Forms;
 
 namespace Group_Project
 {
